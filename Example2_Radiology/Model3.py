771cbe23f7dabddf18ed4b85d971404c9b55b80d
Sara Aghamiri <42683308+SaraAghamiri@users.noreply.github.com>
2024-05-31 23:22:52 -0500

    Create Model3.py

---
+++ Example2_Radiology/Model3.py
@@ -0,0 +1,19 @@
+import os
+import subprocess
+
+def run_script(script_name):
+    result = subprocess.run(['python', script_name], capture_output=True, text=True)
+    print(result.stdout)
+
+if __name__ == "__main__":
+   
+    run_script('PatientsData1.py')
+    
+    
+    run_script('PatientsData2.py')
+    
+  
+    run_script('TrainModel1.py')
+    
+    
+    run_script('Model2.py')
